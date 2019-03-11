2cbd0532130056e27b789babb2bdf70c2a3f5816
Ribeiro, Douglas <Douglas_Ribeiro@Dell.com>
2019-03-11 17:34:27 -0300

    Done!

--- OdeToFood/Program.cs
+++ OdeToFood/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using OdeToFood.Data;
 
 namespace OdeToFood
 {
@@ -16,20 +19,21 @@ namespace OdeToFood
         {
             var host = CreateWebHostBuilder(args).Build();
         
-            MigrateDataBase(host);
+         //   MigrateDataBase(host);
         
             host.Run();
         
         
         }
 
-        private static void MigrateDataBase(IWebHost host)
-        {
-            using(var scope = host.Services.CreateScope())
-            {
-                
-            }
-        }
+     //     private static void MigrateDataBase(IWebHost host)
+     //     {
+     //         using(var scope = host.Services.CreateScope())
+     //         {
+     //                var db = scope.ServiceProvider.GetRequiredService<OdeToFoodDbContext>();
+     //             db.Database.Migrate();
+     //        }
+     //    }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
